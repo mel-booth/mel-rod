db2b418b277f7c528cd4a35ca6e95bd8ecd4c787
mel.m.booth@gmail.com <knfrances@gmail.com>
2016-11-01 11:03:54 +1300

    createCatsTable

--- migrations/20161101105731_createCatsTable.js
+++ migrations/20161101105731_createCatsTable.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTableIfNotExists('cats', function(table){
+    table.increments('id')
+    table.string('name')
+    table.integer('lives')
+    table.text('lifeStory')
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.dropTableIfExists('cats', function(table){
+    table.dropColumn('id')
+    table.string('name')
+    table.integer('lives')
+    table.text('lifeStory')
+  })
+
 };
